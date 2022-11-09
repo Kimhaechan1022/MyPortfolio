edd28a1835ec2dbd6b874a010fbae32204a652c2
KimHaeChan <ckssmsla98@naver.com>
2022-11-10 04:17:27 +0900

    first commit, to connect git hub repository

---
+++ src/main/java/com/myportfolio/MainController.java
@@ -0,0 +1,19 @@
+package com.myportfolio;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index(){
+        return "user/startPage";
+    }
+
+    @GetMapping("/user/register")
+    public String registerPage(){
+        return "user/register";
+    }
+
+}
